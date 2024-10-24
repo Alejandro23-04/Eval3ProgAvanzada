c6da394fa93c05d17f7d84c29cf28e3fba22a10e
Alejandro23-04 <acabrera23@cftsa.cl>
2024-10-24 15:00:41 -0300

    Agregar archivos de proyecto.

---
+++ Eval3ProgAvanzada/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Eval3ProgAvanzada.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
