9ffec10bd5a92f5fde64abaf2475e57ddff60b57
xRichardx995 <rgonzalez22@cftsa.cl>
2024-11-04 11:16:16 -0300

    mensajes de error en "herramienta.cs"

--- Eval3ProgAvanzada/Models/herramienta.cs
+++ Eval3ProgAvanzada/Models/herramienta.cs
@@ -4,29 +4,39 @@ using System.Reflection;
 
 namespace Eval3ProgAvanzada.Models
 {
-    public class herramienta
+    public class Herramienta
     {
         public int Id { get; set; }
 
-        [Required]
-        [ForeignKey("modelo")]
-        public int modeloId { get; set; }
-        public modelo modelo { get; set; }
-        [Required]
+        [Required(ErrorMessage = "El modelo es requerido")]
+        [ForeignKey("Modelo")]
+        public int ModeloId { get; set; }
+
+        public Modelo Modelo { get; set; }  // Propiedad de navegación para Modelo
+
+        [Required(ErrorMessage = "La marca es requerida")]
+        [StringLength(50, ErrorMessage = "La marca no debe superar los 50 caracteres")]
         public string Marca { get; set; }
 
-        [Required]
-        public string Estado { get; set; } // "Disponible", "En Uso", "En Mantención"
+        [Required(ErrorMessage = "El estado es requerido")]
+        [RegularExpression("Disponible|En Uso|En Mantención", ErrorMessage = "Estado no válido")]
+        public string Estado { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "El número de serie es requerido")]
+        [StringLength(100, ErrorMessage = "El número de serie no debe superar los 100 caracteres")]
         public string NumeroSerie { get; set; }
 
-        public DateTime FechaIngreso { get; set; }
-
+        [Range(1, int.MaxValue, ErrorMessage = "Cantidad debe ser mayor a cero")]
         public int CantidadTotal { get; set; }
-        public int CantidadDisponible { get; set; }
-        public int CantidadEnUso { get; set; }
-        public int CantidadEnMantencion { get; set; }
+    }
+    public class Modelo
+    {
+        public int Id { get; set; }
 
+        [Required(ErrorMessage = "El nombre del modelo es requerido")]
+        [StringLength(50)]
+        public string Nombre { get; set; } // Puedes agregar otras propiedades según sea necesario
     }
 }
+
+
